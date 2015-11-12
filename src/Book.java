381242da77fcef6b7f948f66d598c75263ddd9d1
Rotem <rotemfridman@gmail.com>
2015-11-12 17:02:03 +0200

    Revert "Revert "Created the abstract Book class""
    This reverts commit 2a140598b254f0871bc34428416260c0f45694cf.

---
+++ src/Book.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Rotem on 12/11/2015.
+ */
+public abstract class Book extends WrittenWork {
+
+    protected String author;
+
+    protected int pubYear;
+
+    protected int edition;
+
+    protected Book(String author, int pubYear, int edition, String title, double price, String description, Genre genre) {
+        super(title, price, description, genre);
+        this.author = author;
+        this.pubYear = pubYear;
+        this.edition = edition;
+    }
+}
