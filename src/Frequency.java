b15873960a1defb53643507393d032d6de380a86
reuthamou <reuthamou@gmail.com>
2015-11-24 01:09:00 +0200

    not done

--- src/Frequency.java
+++
@@ -1,6 +0,0 @@
-/*
- * The possible frequencies for a periodical
- */
-public enum Frequency {
-    DAILY, WEEKLY, MONTHFY, QUARTERLY, YEARLY
-}
--- src/StandAlone.java
+++
@@ -1,8 +0,0 @@
-
-public class StandAlone extends Book {
-
-    public StandAlone(String author, int pubYear, int edition, String title, double price, String description, Genre genre) {
-        super(author, pubYear, edition, title, price, description, genre);
-    }
-
-}
